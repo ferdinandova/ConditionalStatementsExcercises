d9aeb9075adfce03e4b4a4d322b839f1781893f0
ferdinandova <ferdinandova.ivanaa@gmail.com>
2021-12-07 17:21:51 +0200

    Switch case -> Lambda

--- src/com/company/DayOfWeek.java
+++ src/com/company/DayOfWeek.java
@@ -9,31 +9,14 @@ public class DayOfWeek {
         int dayOfWeek = Integer.parseInt(scanner.nextLine());
 
         switch (dayOfWeek) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
-            default:
-                System.out.println("Try again and enter a number between 1 and 7.");
-                break;
-
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("Try again and enter a number between 1 and 7.");
         }
 
     }
